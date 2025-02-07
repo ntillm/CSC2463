85043b8343f08af91354a8e5337e8c1986e3a198
ntillm <ntillm3@lsu.edu>
2025-02-06 21:55:50 -0600

    finished paint app

--- prog2/sketch.js
+++ prog2/sketch.js
@@ -1,7 +1,60 @@
+let x = 200, y = 200;
+let dragging = false;
+let selectedColor = 'white';
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
+  strokeWeight(10);
 }
 
 function draw() {
-  background(220);
+  drawColorBoxes();
+}
+
+function drawColorBoxes() {
+  noStroke();
+  const colors = [
+    'red', 'orange', 'yellow', 'green', 'cyan',
+    'blue', 'magenta', 'saddlebrown', 'white', 'black'
+  ];
+  const boxSize = 30;
+  const startX = 8;
+  let startY = 10;
+
+  for (let i = 0; i < colors.length; i++) {
+    fill(colors[i]);
+    square(startX, startY, boxSize);
+    startY += boxSize + 2;// Add spacing between boxes
+  }
+}
+
+function mousePressed() {
+  const boxSize = 30;
+  const startX = 8;
+  let startY = 10;
+
+  const colors = [
+    'red', 'orange', 'yellow', 'green', 'cyan',
+    'blue', 'magenta', 'saddlebrown', 'white', 'black'
+  ];
+
+  // Check which color box was clicked
+  for (let i = 0; i < colors.length; i++) {
+    if (
+      mouseX >= startX &&
+      mouseX <= startX + boxSize &&
+      mouseY >= startY &&
+      mouseY <= startY + boxSize
+    ) {
+      selectedColor = colors[i];// Update the selected color
+      console.log(" " + selectedColor);
+      break;
+    }
+    startY += boxSize + 2; // Move to the next box
+  }
+}
+
+function mouseDragged(){
+  stroke(selectedColor);
+  line(pmouseX, pmouseY, mouseX, mouseY);
 }
